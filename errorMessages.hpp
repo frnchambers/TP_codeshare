5acfe69d715c96231bc558cd3e01c034fa758800
Kevin Mooney <mooneykn@tcd.ie>
2016-06-17 16:32:55 +0100

    Error messages macro for c++

---
+++ errorMessages.hpp
@@ -0,0 +1,19 @@
+/******************************************************
+ * Name    : errorMessages.hpp
+ * Author  : Kevin Mooney
+ * Created : 17/06/16
+ * Updated :
+ *
+ * Description:
+ *    Macros for handeling errors 
+ ******************************************************/
+
+#include <stdexcept>
+#include <sstream>
+
+#define errorM(error) { printError((error),__FILE__,__LINE__); }
+inline void printError(const char *str,const char *file, int line ) {
+  std::ostringstream oss;
+  oss << file << ":" <<  line << ": " << str;
+  throw std::logic_error ( oss.str() );
+}
